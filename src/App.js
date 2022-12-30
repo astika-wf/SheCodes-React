bcf4e59e0586721333651fc82734792a9fc3d9ee
Astika <105666996+astika-wf@users.noreply.github.com>
2022-12-30 23:47:21 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,7 +11,9 @@ export default function App() {
         <div className="container">
           <Weather defaultCity="Jakarta" />
         </div>
+        <div className="github">
         <GithubRepo />
+        </div>
       </div>
     </div>
   );
