b4fbcef7e3ca39c98fa9dd34237e5b73c03c9bc8
Astika <105666996+astika-wf@users.noreply.github.com>
2022-12-30 23:07:04 +0700

    Update WeatherForecastDay.js

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import WeatherIcon from "./WeatherIcon";
+import WeatherIcon from "./Icon";
 
 export default function WeatherForecastDay(props) {
   function maxTemperature() {
