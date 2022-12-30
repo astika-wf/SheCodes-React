ba6b45a6980599c2a4fb42966a9a3e3143b2764f
Astika <105666996+astika-wf@users.noreply.github.com>
2022-12-30 22:38:08 +0700

    Update WeatherForecastDay.js

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -24,7 +24,7 @@ export default function WeatherForecastDay(props) {
         <div className="WeatherForecast-day mb-2">{day()}</div>
         <WeatherIcon code={props.data.weather[0].icon} size={34} />
         <div className="WeatherForecast-temperatures mt-2">
-        <span className="WeatherForecast-temperature-max">{maxTemperature()}</span>
+        <span className="WeatherForecast-temperature-max">{maxTemperature()}</span> |
         <span className="WeatherForecast-temperature-min">{minTemperature()}</span>
         </div>
     </div>
